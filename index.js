109dec10a186313286a939706b65cf802b19e7e9
cosima <xilllll@126.com>
2022-08-13 13:17:50 +0800

    feat: change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const crypto = require('crypto');
-const { parse } = require('url'); 
+const { parse } = require('url');
 
 // 缓存需要推送的信息
 const datas = [];
@@ -12,7 +12,6 @@ const callbacks = {};
 const server = http.createServer().listen(3000);
 server.on('request', (req, res) => {
   const { pathname, query } = parse(req.url, true);
-  console.log(pathname, 'pathname-----');
   if (pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
     return res.end(fs.readFileSync('static/index.html'));
@@ -72,7 +71,7 @@ server.on('request', (req, res) => {
     });
     return;
   }
-
+  console.log(datas, callbacks,  'pathname-----');
   res.write('hello world');
   res.end();
 });
@@ -80,9 +79,9 @@ server.on('request', (req, res) => {
 server.on('upgrade', (req, socket) => {
   const cbk = 'ws';
   delete callbacks[cbk];
-  const acceptKey = req.headers['sec-websocket-key']; 
-  const hash = generateAcceptValue(acceptKey); 
-  const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` ];
+  const acceptKey = req.headers['sec-websocket-key'];
+  const hash = generateAcceptValue(acceptKey);
+  const responseHeaders = ['HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}`];
   socket.write(responseHeaders.join('\r\n') + '\r\n\r\n');
   socket.write(constructReply(datas));
   callbacks[cbk] = (d) => {
@@ -95,9 +94,9 @@ server.on('upgrade', (req, socket) => {
 
 function generateAcceptValue (acceptKey) {
   return crypto
-  .createHash('sha1')
-  .update(acceptKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', 'binary')
-  .digest('base64');
+    .createHash('sha1')
+    .update(acceptKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', 'binary')
+    .digest('base64');
 }
 
 function constructReply (data) {
@@ -105,19 +104,19 @@ function constructReply (data) {
   const json = typeof data === 'string' ? data : JSON.stringify(data);
   const jsonByteLength = Buffer.byteLength(json);
   // Note: we're not supporting > 65535 byte payloads at this stage 
-  const lengthByteCount = jsonByteLength < 126 ? 0 : 2; 
-  const payloadLength = lengthByteCount === 0 ? jsonByteLength : 126; 
-  const buffer = Buffer.alloc(2 + lengthByteCount + jsonByteLength); 
+  const lengthByteCount = jsonByteLength < 126 ? 0 : 2;
+  const payloadLength = lengthByteCount === 0 ? jsonByteLength : 126;
+  const buffer = Buffer.alloc(2 + lengthByteCount + jsonByteLength);
   // Write out the first byte, using opcode `1` to indicate that the message 
   // payload contains text data 
-  buffer.writeUInt8(0b10000001, 0); 
-  buffer.writeUInt8(payloadLength, 1); 
+  buffer.writeUInt8(0b10000001, 0);
+  buffer.writeUInt8(payloadLength, 1);
   // Write the length of the JSON payload to the second byte 
-  let payloadOffset = 2; 
-  if (lengthByteCount > 0) { 
-    buffer.writeUInt16BE(jsonByteLength, 2); payloadOffset += lengthByteCount; 
-  } 
+  let payloadOffset = 2;
+  if (lengthByteCount > 0) {
+    buffer.writeUInt16BE(jsonByteLength, 2); payloadOffset += lengthByteCount;
+  }
   // Write the JSON data to the data buffer 
-  buffer.write(json, payloadOffset); 
+  buffer.write(json, payloadOffset);
   return buffer;
 }
